fa771d4fc0bc0e519d27a5ce619ef9847d9ee28e
Vaishali Verma <62252567+vvaishali519@users.noreply.github.com>
2023-03-10 11:00:58 +0530

    First Commit

---
+++ src/com/app/utils/JdbcUtil.java
@@ -0,0 +1,37 @@
+package com.app.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JdbcUtil {
+
+	private JdbcUtil() {
+	}
+
+	static {
+		// Step1: loading and register the Driver
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException ce) {
+			ce.printStackTrace();
+		}
+	}
+
+	public static Connection getJdbcConnection() throws SQLException, IOException {
+		String fileLoc = "D:\\iNeuron\\Projects\\OnlineBookShop\\src\\com\\app\\properties\\application.properties";
+		FileInputStream fis = new FileInputStream(fileLoc);
+		Properties properties = new Properties();
+		properties.load(fis);
+
+		String url = properties.getProperty("jdbcUrl");
+		String username = properties.getProperty("user");
+		String password = properties.getProperty("password");
+
+		return DriverManager.getConnection(url, username, password);
+	}
+
+}
